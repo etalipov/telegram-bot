23d140d98a708d8241262e5a2b39d9f1513262c9
etalipov <34828350+etalipov@users.noreply.github.com>
2025-01-08 15:34:54 +0400

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ from os import environ
 print("Start")
 
 a = environ.get("KANDINSKIY_API_KEY")
-print(type(a))
+print(a)
   
 print(environ.get("KANDINSKIY_SECRET_KEY"))
+print(environ.get("TEST"))
 
 print("End")
